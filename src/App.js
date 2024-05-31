bacac128a003cef3106763c1cb5cbd8a36cb207f
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2024-05-31 13:09:37 +0700

    Update: Add glob

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/global/Navbar";
+import HomePage from "./pages/HomePage";
+import Footer from "./components/global/Footer";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      {
+        // Routes
+        <HomePage />
+      }
+      <Footer />
+    </>
+  );
 }
 
 export default App;
