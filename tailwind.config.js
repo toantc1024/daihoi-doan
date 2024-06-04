97d4532af8500b867ca8c20b202255812ac9c03b
AndyAnh <hovietanh147@gmail.com>
2024-06-04 14:11:49 +0700

    Coutdown done and customcarousel (em khong chinh dc anh trong)

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,17 @@ module.exports = {
         DEFAULT: "0 2px 4px var(--tw-shadow-color)",
         lg: "0 8px 16px var(--tw-shadow-color)",
       },
+      
+    },
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
     },
   },
   plugins: [
