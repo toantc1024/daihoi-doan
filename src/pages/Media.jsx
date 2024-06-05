7fdb4317a81b236fefcda94a1f9398b9a3e00f95
AndyAnh <hovietanh147@gmail.com>
2024-06-05 11:33:03 +0700

    udapte: media & media1

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
 
-const Media = () => {
-  return <div>Media</div>;
-};
+function Media() {
+  return (
+    <div className="container mx-auto p-4">
+      <div className="flex flex-col gap-4">
+        <div className="w-full md:w-full">
+          <video controls className="w-full h-80 md:h-3/4">
+            <source src="https://www.youtube.com/watch?v=dQw4w9WgXcQ" type="video/mp4" /> {/* lấy video từ youtube*/}
+          </video>
+        </div>
+        <div className="w-full md:w-full">
+          <img src="https://picsum.photos/id/237/400/250" alt="Poster" className="w-full h-48 md:h-1/3 object-cover" /> {/*chưa biết kích thước chuẩn poster*/}
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Media;
---
+++ src/pages/Media2.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Media2 = () => {
+  return <div>Media</div>;
+};
+
+export default Media2;
